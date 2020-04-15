9129a51066f92a22162944d57baa874cca405507
Pawel <jarrodek@gmail.com>
2020-04-15 13:47:09 -0700

    chore: updating project's config for eslint and prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,1 @@
-module.exports = {
-  printWidth: 120,
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'none',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  arrowParens: 'always',
-  quoteProps: 'consistent'
-};
+module.exports = require('@advanced-rest-client/prettier-config');
